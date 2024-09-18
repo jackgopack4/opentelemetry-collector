5c5a6328a35608be64b9cb7a1e89fefefa1e561f
jackgopack4 <john.peterson@datadoghq.com>
2024-09-18 10:52:39 -0400

    Revert "[chore] delete code to set a version and date, as it it not used (#10715)"
    This reverts commit b53f57dc89a7317db303ab45ca821dd06ee102d1.

--- cmd/builder/internal/command.go
+++ cmd/builder/internal/command.go
@@ -123,7 +123,7 @@ configuration is provided, ocb will generate a default Collector.
 
 func initConfig(flags *flag.FlagSet) error {
 	cfg.Logger.Info("OpenTelemetry Collector Builder",
-		zap.String("version", version))
+		zap.String("version", version), zap.String("date", date))
 
 	var provider koanf.Provider
 
